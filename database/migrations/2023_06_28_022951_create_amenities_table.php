2ce5df5df7f081349bdd7ba4bd5d68f9367e3d8b
Muhammad Ridho Heriansyah <ridhoheriansyah1@gmail.com>
2023-06-28 10:59:11 +0700

    soft delete

--- database/migrations/2023_06_28_022951_create_amenities_table.php
+++ database/migrations/2023_06_28_022951_create_amenities_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name', 100);
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
